0fba6b4505b3455c4a795f404a1b9ea95ae1cb96
Vincent Nwonah <vnwonah@outlook.com>
2019-11-29 22:46:43 +0200

    Add project files.

---
+++ App/App.Android/MainActivity.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using App.Droid.Services;
+using System.Threading.Tasks;
+using Android;
+using Android.Support.V4.App;
+using System.Linq;
+using Android.Content;
+
+namespace App.Droid
+{
+    [Activity(Label = "App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public static long DownloadId;
+        public static string FileName;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.SetVmPolicy(builder.Build());
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            MessagingCenter.Subscribe<Xamarin.Forms.Button>(this, "LAUNCHINSTALLER", (sender) =>
+            {
+                if(haveStoragePermission() && hasRequestInstallPermisison())
+                {
+                    DownloadAndInstall();
+                }
+            });
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if(grantResults.Any(r => r == Permission.Granted))
+            {
+                MessagingCenter.Send<Xamarin.Forms.Button>(new Xamarin.Forms.Button(), "LAUNCHINSTALLER");
+            }
+        }
+
+        public void DownloadAndInstall()
+        {
+            var manager = DownloadManager.FromContext(this); //(DownloadManager)this.GetSystemService(Android.Content.Context.DownloadService);
+            var iservice = new InstallService();
+            Task.Run(async () => await iservice.DownloadAndInstall(manager));
+        }
+
+        public bool hasRequestInstallPermisison()
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                if (CheckSelfPermission(Android.Manifest.Permission.RequestInstallPackages) == Permission.Granted)
+                {
+                    return true;
+                }
+                else
+                {
+
+                    ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.RequestInstallPackages }, 1);
+                    return false;
+                }
+            }
+            else
+            { //you dont need to worry about these stuff below api level 23
+                return true;
+            }
+        }
+        
+        public bool haveStoragePermission()
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                if (CheckSelfPermission(Android.Manifest.Permission.WriteExternalStorage) == Permission.Granted)
+                {
+                    DownloadAndInstall();
+                    return true;
+                }
+                else
+                {
+
+                    ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.WriteExternalStorage }, 1);
+                    return false;
+                }
+            }
+            else
+            { //you dont need to worry about these stuff below api level 23
+                return true;
+            }
+        }
+
+    }
+
+
+}
---
+++ App/App.Android/Services/InstallService.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Webkit;
+using Java.IO;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App.Droid.Services
+{
+    public class InstallService
+    {
+        public async Task DownloadAndInstall(DownloadManager manager)
+        {
+
+            string url = @"https://app.cforce.live/com.payforce.apk";
+            var source = Android.Net.Uri.Parse(url);
+            var request = new DownloadManager.Request(source);
+            request.AllowScanningByMediaScanner();
+            request.SetAllowedOverRoaming(true);
+            request.SetDescription("Downloading PayForce");
+            request.SetAllowedOverMetered(true);
+            request.SetVisibleInDownloadsUi(true);
+            string filename = URLUtil.GuessFileName(url, null, MimeTypeMap.GetFileExtensionFromUrl(url));
+            MainActivity.FileName = filename;
+            request.SetDestinationInExternalPublicDir(Android.OS.Environment.DirectoryDownloads, filename);
+            request.SetNotificationVisibility(DownloadManager.Request.VisibilityVisibleNotifyCompleted);
+            request.SetAllowedNetworkTypes(DownloadNetwork.Wifi | DownloadNetwork.Mobile);
+            MainActivity.DownloadId = manager.Enqueue(request);
+        }
+    }
+
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new[] { DownloadManager.ActionDownloadComplete })]
+    public class DownloadReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            // Do stuff here.
+            long id = intent.GetLongExtra(DownloadManager.ExtraDownloadId, -1);
+            if(id == MainActivity.DownloadId)
+            {
+                Intent installIntent = new Intent(Intent.ActionInstallPackage);
+                installIntent.SetDataAndType(Android.Net.Uri.FromFile(new File(Environment.GetExternalStoragePublicDirectory(Environment.DirectoryDownloads) +"/"+ MainActivity.FileName)), "application/vnd.android.package-archive");
+                installIntent.SetFlags(ActivityFlags.NewTask);
+                Forms.Context.StartActivity(installIntent);
+            }
+        }
+    }
+}
